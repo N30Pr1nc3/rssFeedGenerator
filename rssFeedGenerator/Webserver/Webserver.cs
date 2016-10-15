277ef427eee908d1f84f8dec7e4179e98dced90b
N30Pr1nc3 <tobiasstanitzek@outlook.com>
2016-10-15 22:18:39 +0200

    webserver cs vergessen

---
+++ rssFeedGenerator/Webserver/Webserver.cs
@@ -0,0 +1,81 @@
+﻿// MIT License - Copyright (c) 2016 Can Güney Aksakalli
+
+using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+using System.Threading;
+using rssFeedGenerator;
+
+class SimpleHTTPServer
+{
+    private Thread _serverThread;
+
+    /// <summary>
+    /// Construct server with given port.
+    /// </summary>
+    /// <param name="path">Directory path to serve.</param>
+    /// <param name="port">Port of the server.</param>
+    public SimpleHTTPServer(string path, int port)
+    {
+        this.Initialize(path, port);
+    }
+
+    public void Listen()
+    {
+        if (!HttpListener.IsSupported)
+        {
+            Console.WriteLine("Windows XP SP2 or Server 2003 is required to use the HttpListener class.");
+            return;
+        }
+        // URI prefixes are required,
+        // for example "http://contoso.com:8080/index/".
+        string[] prefixes = { "http://127.0.0.1:12345/" };
+        if (prefixes == null || prefixes.Length == 0)
+            throw new ArgumentException("prefixes");
+
+        // Create a listener.
+        HttpListener listener = new HttpListener();
+        // Add the prefixes.
+        foreach (string s in prefixes)
+        {
+            listener.Prefixes.Add(s);
+        }
+        listener.Start();
+        Console.WriteLine("Listening...");
+        while (true)
+        {
+            // Note: The GetContext method blocks while waiting for a request. 
+            HttpListenerContext context = listener.GetContext();
+            HttpListenerRequest request = context.Request;
+            // Obtain a response object.
+            HttpListenerResponse response = context.Response;
+            // Construct a response.
+            string responseString = Program.generateRssFeed(request.RawUrl);
+            byte[] buffer = System.Text.Encoding.UTF8.GetBytes(responseString);
+            // Get a response stream and write the response to it.
+            response.ContentLength64 = buffer.Length;
+            System.IO.Stream output = response.OutputStream;
+            output.Write(buffer, 0, buffer.Length);
+            // You must close the output stream.
+            output.Close();
+        }        
+    }
+
+    /// <summary>
+    /// Stop server and dispose all functions.
+    /// </summary>
+    public void Stop()
+    {
+        _serverThread.Abort();
+    }
+
+    private void Initialize(string path, int port)
+    {
+        _serverThread = new Thread(this.Listen);
+        _serverThread.Start();
+    }
+
+
+}
